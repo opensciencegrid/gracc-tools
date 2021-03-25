a1632bd822d15e7d17052166e0624a267f4a6f1b
Derek Weitzel <dweitzel@cse.unl.edu>
2021-03-25 14:07:25 -0500

    Adding removal of local purdue jobs

---
+++ gracc-oneoffs/remove-purdue-local/remote-purdue-local.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import elasticsearch
+from elasticsearch_dsl import Search, A, Q
+#import logging
+import operator
+import sys
+import os
+import dateutil.parser as parser
+
+#logging.basicConfig(level=logging.WARN)
+#es = elasticsearch.Elasticsearch(
+#        ['https://gracc.opensciencegrid.org/q'],
+#        timeout=300, use_ssl=True, verify_certs=False)
+es = elasticsearch.Elasticsearch()
+
+osg_raw_index = 'gracc.osg.raw-*'
+osg_summary_index = 'gracc.osg.summary'
+
+starttime = parser.parse("2021-02-12")
+endtime = parser.parse("2021-03-17")
+
+s = Search(using=es, index=osg_raw_index)
+
+s = s.query("match", Grid="Local")
+s = s.query("match", ProbeName="slurm:hammer-osg.rcac.purdue.edu")
+s = s.query(Q("range", EndTime={"gte": starttime, "lt": endtime}))
+s = s.query(~Q("exists", field="VOName"))
+response = s.execute()
+
+print("Query took %i milliseconds" % response.took)
+
+print("Query got %i hits" % response.hits.total.value)
+#s.delete()
+
+
+s = Search(using=es, index=osg_summary_index)
+s = s.query("match", Grid="Local")
+s = s.query("match", ProbeName="slurm:hammer-osg.rcac.purdue.edu")
+s = s.query(Q("range", EndTime={"gte": starttime, "lt": endtime}))
+s = s.query("match", VOName="N/A")
+
+response = s.execute()
+print("Query took %i milliseconds" % response.took)
+
+print("Query got %i hits" % response.hits.total.value)
+#s.delete()
+
